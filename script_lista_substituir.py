1722d04630a318500884d8401b0d82dad2622df6
inacio88 <inacio88@outlook.com>
2023-07-22 16:26:45 -0300

    removendo strings duplicadas

--- script_lista_substituir.py
+++ script_lista_substituir.py
@@ -1,6 +1,15 @@
 import re
 import argparse
 
+def remove_duplicates(array):
+    # Usar um conjunto para remover elementos duplicados mantendo a ordem original
+    unique_elements = []
+    for item in array:
+        if item not in unique_elements:
+            unique_elements.append(item)
+    return unique_elements
+
+
 def read_file_to_array_sem_linha_vazia(filepath):
     lines_array = []
     try:
@@ -34,12 +43,12 @@ def simples_replace(velha_string, replacement, filepath):
 if __name__ == "__main__":
     entrada_lista_strings = "teste/lista_strings_encontradas.txt"  # Substitua pelo caminho do arquivo desejado
     linhas = read_file_to_array_sem_linha_vazia(entrada_lista_strings)
-
+    linhas_sem_repeticao = remove_duplicates(linhas)
 
     saida_arquivo_onde_tera_substituicao = "exemplo_final.cs"  # Substitua pelo caminho do arquivo desejado
 
 
-    for sentenca in linhas:
+    for sentenca in linhas_sem_repeticao:
         simples_replace(sentenca, sentenca + ".i18n()", saida_arquivo_onde_tera_substituicao)
 
 
